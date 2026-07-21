aab6073c415039d94472d0b3b2db6166320c1a6e
MuratYucedag <yucedagmurat23@gmail.com>
2026-07-21 22:06:53 +0300

    Add project files.

---
+++ Services/TheShop.Discount/Dtos/ResultDiscountCodeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TheShop.Discount.Dtos
+{
+    public class ResultDiscountCodeDto
+    {
+        public int DiscountCodeId { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Services/TheShop.Discount/Entities/DiscountCode.cs
@@ -0,0 +1,10 @@
+﻿namespace TheShop.Discount.Entities
+{
+    public class DiscountCode
+    {
+        public int DiscountCodeId { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
